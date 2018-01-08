de09c5bf0c537f903f69c7b476ea7a783dc81087
gabrielsaruhashi <gabriel.saruhashi@gmail.com>
2018-01-08 16:40:12 -0200

    Installation

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask
 import requests
 from lxml import html
-from bs4 import BeautifulSoup
-
 
 app = Flask(__name__)
 
@@ -13,50 +11,19 @@ def hello_world():
 
 @app.route('/scrape')
 def scrape():
-    # login session persists
-    #session_requests = requests.session()
+    session_requests = requests.session()
 
-    #auth = $("[name=authenticity_token], body").val;
-
-
-
-    # perf
-    login_url = "https://auth.aiesec.org/users/sign_in"
-    page = requests.get(login_url)
-    
-    print page
-
-    root = html.parse(page)
-    auth_token = root.xpath("//input[@name='authenticity_token']/@value")[0]
-    print auth_token;
-
-    '''
+    # Get login csrf token
+    result = session_requests.get("https://auth.aiesec.org/users/sign_in")
     tree = html.fromstring(result.text)
-    console.log(tree);
     authenticity_token = list(set(tree.xpath("//input[@name='authenticity_token']/@value")))[0]
-    
-
-
-        # login information
+    print authenticity_token
+    # Create payload
     payload = {
-        "user[email]": "gabriel.saruhashi@yale.edu", 
-        "password": "Raptores1", 
-        "commit": 'Sign in',
-        "authenticity_token": $("[name=authenticity_token]", body).val()
-    }
+        "username": USERNAME, 
+        "password": PASSWORD, 
+        "csrfmiddlewaretoken": authenticity_token
+    }'''
+    return "Scraping"
 
-    result = session_requests.post(
-	login_url, 
-	data = payload, 
-	headers = dict(referer=login_url)
-    )
-
-    console.log(result);
-
-    #url = 'https://bitbucket.org/dashboard/overview'
-   # result = session_requests.get(
-	#url, 
-	#headers = dict(referer = url)
-    )
-        '''
 
